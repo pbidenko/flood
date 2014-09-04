42a97914f08c6203c2e5ea0540b8848b8513b9e6
vtaran <vtaran@sitrusllc.com>
2014-09-04 15:05:58 +0300

    added by mistake

--- app/scripts/models/messages/Message.js
+++
@@ -1,9 +0,0 @@
-define(function(){
-    function Message(commands, guid){
-        this.$type = 'Dynamo.Messages.Message, DynamoCore';
-        this.commands = commands || [];
-		this.WorkspaceGuid = guid;
-    }
-
-    return Message;
-});
