9295f6cd627df4433f2e774cbd961aad91ab2953
pbidenko <pavel.bidenko@gmail.com>
2014-11-21 15:28:03 +0200

    REACH-111 No geometry shown for Code Block nodes

--- app/scripts/views/NodeViews/CodeBlock.js
+++ app/scripts/views/NodeViews/CodeBlock.js
@@ -1,12 +1,12 @@
-define(['backbone', 'BaseNodeView'], function (Backbone, BaseNodeView) {
+define(['backbone', 'ThreeCSGNodeView'], function (Backbone, ThreeCSGNodeView) {
 
-    var CodeBlock = BaseNodeView.extend({
+    var CodeBlock = ThreeCSGNodeView.extend({
 
         innerTemplate: _.template($('#code-block-template').html()),
 
         initialize: function (args) {
 
-            BaseNodeView.prototype.initialize.apply(this, arguments);
+            ThreeCSGNodeView.prototype.initialize.apply(this, arguments);
             this.model.on('change:extra', this.onChangedExtra, this);
             this.model.on('connections-update', this.onConnectionsUpdate, this);
             this.model.on('cbn-up-to-date', this.finishEvaluating, this);
@@ -208,7 +208,7 @@ define(['backbone', 'BaseNodeView'], function (Backbone, BaseNodeView) {
                 margintop,
                 port;
 
-            BaseNodeView.prototype.renderNode.apply(this, arguments);
+            ThreeCSGNodeView.prototype.renderNode.apply(this, arguments);
 
             this.input = this.$el.find('.code-block-input');
 
@@ -275,7 +275,7 @@ define(['backbone', 'BaseNodeView'], function (Backbone, BaseNodeView) {
         },
 
         moveNode: function() {
-            BaseNodeView.prototype.moveNode.apply(this, arguments);
+            ThreeCSGNodeView.prototype.moveNode.apply(this, arguments);
 
             if(!this.input[0].value){
                 this.input.focus();
@@ -286,7 +286,7 @@ define(['backbone', 'BaseNodeView'], function (Backbone, BaseNodeView) {
 
         render: function () {
 
-            BaseNodeView.prototype.render.apply(this, arguments);
+            ThreeCSGNodeView.prototype.render.apply(this, arguments);
 
             return this;
 
