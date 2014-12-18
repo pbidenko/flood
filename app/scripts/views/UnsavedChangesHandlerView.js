00a44d76fe857ef0b85cba4120fba83a532a908f
marimano <maria.manoylo@autodesk.com>
2014-12-18 18:15:25 +0200

    make the dialog work correctly on closing by X button, make it nicer and put in the center of the page

--- app/scripts/views/UnsavedChangesHandlerView.js
+++ app/scripts/views/UnsavedChangesHandlerView.js
@@ -3,6 +3,35 @@
  */
 define(['backbone', 'jqueryuidialog'], function(Backbone, jqueryuidialog) {
 
+    var dialogConfig = {
+        iconHtml: '<i class="fa fa-times confirm-dialog-icon" style="margin-left:1px;"></i>',
+        class: 'confirm-window',
+        xButtonClass: '.ui-dialog-titlebar-close',
+        width: 330,
+        textSelector: '#dialog-confirm-text'
+    };
+
+    function setContent (name) {
+        var dialogContent = 'You have unsaved changes to ' + name +
+            '.<br/><br/> Would you like to save your changes?';
+        $(dialogConfig.textSelector).html(dialogContent);
+    }
+
+    function getDialog () {
+        return $('.' + dialogConfig.class);
+    }
+
+    function setPositionAndCloseIcon () {
+        $(dialogConfig.xButtonClass).html(dialogConfig.iconHtml);
+
+        var dialog = getDialog();
+        // coordinates to put the dialog right into the page center
+        var x = ($(window).width() - dialog.width()) / 2,
+            y = ($(window).height() - dialog.height()) / 2;
+
+        dialog.css({ top: y, left: x });
+    }
+
     return Backbone.View.extend({
 
         initialize: function() {
@@ -10,7 +39,7 @@ define(['backbone', 'jqueryuidialog'], function(Backbone, jqueryuidialog) {
         },
 
         askForSaving: function (data) {
-            var path, fileName, dialogText,
+            var path, fileName,
                 ws, saveUploader,
                 model = this.model;
             model.savingData = data;
@@ -24,30 +53,37 @@ define(['backbone', 'jqueryuidialog'], function(Backbone, jqueryuidialog) {
                     fileName = ws.get('name') + ' workspace';
                 }
 
-                dialogText = "You have unsaved changes to " + fileName +
-                    ".\n\n Would you like to save your changes?";
-
                 model.app.set('currentWorkspace', ws.get('_id'));
 
-                $('#dialog-confirm-text').html(dialogText);
+                var resetAction = true;
+                setContent(fileName);
                 var dialog = $( '#dialog-confirm' ).dialog({
                     modal: true,
+                    width: dialogConfig.width,
+                    dialogClass: dialogConfig.class,
                     buttons: {
                         "Yes": function() {
+                            resetAction = false;
                             dialog.dialog('close');
                             // action will be continued after saving is done
                             saveUploader.trySaveFile();
                         },
                         "No":  function() {
+                            resetAction = false;
                             dialog.dialog('close');
                             model.continueAction();
                         },
                         "Cancel":  function() {
                             dialog.dialog('close');
-                            model.resetAction();
                         }
+                    },
+                    close: function() {
+                        if (resetAction)
+                            model.resetAction();
                     }
                 });
+
+                setPositionAndCloseIcon();
             }
             else {
                 model.continueAction();
