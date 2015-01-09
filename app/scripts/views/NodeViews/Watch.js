32d0cdc8f0cd17ed124b3dee34feac2cf3652551
pbidenko <pavel.bidenko@gmail.com>
2015-01-09 11:00:41 +0200

    watch node rework

--- app/scripts/views/NodeViews/Watch.js
+++ app/scripts/views/NodeViews/Watch.js
@@ -1,39 +1,67 @@
-define(['backbone', 'underscore', 'jquery', 'BaseNodeView'], function(Backbone, _, $, BaseNodeView) {
+define(['backbone', 'underscore', 'jquery', 'BaseNodeView'], function (Backbone, _, $, BaseNodeView) {
 
-  return BaseNodeView.extend({
+    return BaseNodeView.extend({
 
-    template: _.template( $('#node-watch-template').html() ),
+        template: _.template($('#node-watch-template').html()),
 
-    initialize: function(args) {
+        initialize: function (args) {
 
-      BaseNodeView.prototype.initialize.apply(this, arguments);
-      this.model.on( 'change:lastValue', this.renderNode, this );
-      this.model.on( 'disconnection', this.renderNode, this );
+            BaseNodeView.prototype.initialize.apply(this, arguments);
+            this.model.on('change:lastValue', this.renderNode, this);
+            this.model.on('disconnection', this.renderNode, this);
 
-    },
+        },
 
-    renderNode: function(){
+        renderNode: function () {
 
-    	var pretty = this.model.get('lastValue') != undefined ? JSON.parse(JSON.stringify(this.model.get('lastValue'), this.prettyPrint, 2)) : this.model.get('lastValue');
-    	this.model.set('prettyValue', pretty );
+            var pretty = this.model.get('lastValue') != undefined ? this.prettyPrint(this.model.get('lastValue')) : this.model.get('lastValue');
 
-    	return BaseNodeView.prototype.renderNode.apply(this, arguments);
+            this.model.set('prettyValue', pretty);
 
-    },
+            var json = this.model.toJSON();
 
-    prettyPrint: function(key, val){
+            this.$el.html(this.template(json));
 
-    	if (typeof val === "number"){
-    		return val.toPrecision(4);
-    	}
+            return this;
 
-    	if (typeof val === "string"){
-    		return val.replace(new RegExp("\t", 'g'), "&#09;").replace(new RegExp("\n", 'g'), "<br>");
-    	}
+        },
 
-    	return val;
-    }
+        prettyPrint: function (data) {
+            var val = JSON.parse(data);
 
-  });
+            if (typeof val === 'number') {
+                return val.toPrecision(4);
+            }
+
+            if (typeof val === 'string') {
+                return val.replace(new RegExp('\t', 'g'), '').replace(new RegExp('\n', 'g'), '<br>');
+            }
+
+            if ($.isArray(val)) {
+                return this.parseArray(val, 2);
+            }
+
+            return val;
+        },
+
+        parseArray: function (array, index) {
+            var tab = Array(index).join('&#09;');
+            var result = array.length ? 'List: <br>' : 'Empty List';
+
+            for (var i = 0; i < array.length; i++) {
+                result += tab + '[' + i + '] ';
+
+                if ($.isArray(array[i])) {
+                    result += this.parseArray(array[i], index + 1);
+                }
+                else {
+                    result += array[i] + '<br>';
+                }
+            }
+
+            return result;
+        }
+
+    });
 
 });
